97210985e7aa6c131ebc6308f8092f628e0b0d00
mango <cyc5454@gmail.com>
2021-03-03 01:46:20 +0900

    3.3. 불법사이트와 연결된 사이트 저장 코드 작성

--- crawler/spiders/sites.py
+++ crawler/spiders/sites.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from datetime import datetime as dt
-from util.utils import get_ip, initialize_database, select_all_fullurls
+from urllib.parse import urlparse
+from util.utils import get_ip, initialize_database, select_all_fullurls, initialize_database2
 
 class SitesPySpider(scrapy.Spider):
     name = 'sites'
@@ -14,21 +15,70 @@ class SitesPySpider(scrapy.Spider):
     def start_requests(self):
         dbConnect = initialize_database("illegals.db")
         self.start_urls = select_all_fullurls(dbConnect)
-        #self.start_urls = ['https://mangacat2.net','http://funbe.yoga','http://ww38.bomix2.com','https://copytoon111.com']
+        self.tmpStore = {}
+        self.dbConnectStore = initialize_database2('sites_connection.db') # 결과물 DB 저장 초기화
         for url in self.start_urls:
-            yield scrapy.Request( url = url, callback= self.linkparse, method='GET', encoding = 'utf=8')
+            yield scrapy.Request( url = url, callback= self.link_parse, method='GET', encoding = 'utf=8')
+        
 
-    def linkparse(self, response):
+    def link_parse(self, response):
         baseIP = get_ip(response.url)
         print(response.url, baseIP)
-        links = response.xpath('//a/@href').re(r'http.*')
-        ips = []
+        links = response.xpath('//a/@href').re(r'http.*') # ToDo : banner link만 수집할 수 있도록 수정
         for link in links:
             try:
                 ip = get_ip(link)
-                if (ip is not baseIP) and (ip not in ips): #멀티프로세싱으로 바꾸면 자신의 부모 IP가져오게 만들어야 함
-                    ips.append(ip)
-                    print(ip, link)
+                if not self.db_check(self.dbConnectStore, baseIP, ip): # DB 확인 후 같은 노드 없으면 추가
+                    row = {'main_url':urlparse(response.url).netloc,'main_ip':baseIP,'connect_url':urlparse(link).netloc,'connect_ip':ip}
+                    with self.dbConnectStore:
+                        cursor = self.dbConnectStore.cursor()
+                        sql = f"""
+                            INSERT INTO sites_connection VALUES (
+                                \'{row["main_url"]}\',
+                                \'{row["main_ip"]}\',
+                                \'{row["connect_url"]}\',
+                                \'{row["connect_ip"]}\'
+                            )
+                        """
+                        
+                        cursor.execute(sql)
+                        self.dbConnectStore.commit()
+                    print("[+] db store : %s, %s" % (baseIP, ip))
+            except Exception as e:
+                print(e)
+                continue
+        yield links
+
+    def link_parse2(self, response): #임시 테스트 결과 메모리저장
+        baseIP = get_ip(response.url)
+        print(response.url, baseIP)
+        links = response.xpath('//a/@href').re(r'http.*')
+        for link in links:
+            try:
+                ip = get_ip(link)
+                if (baseIP in self.tmpStore.keys()) and (ip not in self.tmpStore[baseIP]):
+                    self.tmpStore[baseIP].append(ip)
+                    print("[+] db store : %s, %s" % (baseIP, ip))
+                else:
+                    self.tmpStore[baseIP] = [ip,]
             except:
                 continue
-        yield ips
+        yield self.tmpStore
+
+    def db_check(self, dbConnect, mainIP, connectIP):
+        try:
+            with dbConnect:
+                cursor = dbConnect.cursor()
+
+                sql = f"""
+                    SELECT 1
+                    FROM sites_connection
+                    WHERE main_ip = \'{mainIP}\' AND connect_ip = \'{connectIP}\'
+                """ # 조건만족시 1 반환
+
+                result = cursor.execute(sql)
+
+                dbConnect.commit()
+        except Exception as e:
+                print(e)
+        return True if len(result.fetchall()) > 0 else False
