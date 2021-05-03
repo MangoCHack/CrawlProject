0d9d2f015fb441680ca9e658bc52ee31a03ed8c8
mango <cyc5454@gmail.com>
2021-05-03 12:14:37 +0900

    5.3 github restore

--- crawler/items.py
+++ crawler/items.py
@@ -5,8 +5,13 @@
 
 import scrapy
 
-
 class CrawlerItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    main_url = scrapy.Field()
+    main_ip = scrapy.Field()
+    connect_url = scrapy.Field()
+    connect_ip = scrapy.Field()
+    keywords = scrapy.Field()
+    banner_count = scrapy.Field()
     pass
+    
